ac04ab50badb4732858f4b6fa9f0b9c9d6fd9ecd
Taylor Cummings <tdcummings5@gmail.com>
2022-11-30 10:21:51 -0600

    commit tree 41128bf581d6f9323073bb5ad661035fcbff11d9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// write your code here
+<form id="edit-ramen">
+  <h4>Update the Featured Ramen</h4>
+  <label for="rating">Rating: </label>
+  <input type="number" name="rating" id="new-rating" />
+  <label for="new-comment">Comment: </label>
+  <textarea name="Yummy!" id="Yummy!"></textarea>
+  <input type="submit" value="Update" />
+</form>
